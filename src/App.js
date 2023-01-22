3277e8c28213a05cd68c7903cb07ec50a88f8e03
jelena <repac01jelena@gmail.com>
2023-01-22 22:04:10 +0100

    Added navBar component and css for navBar

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import logo from './images/arqa.jpg';
 import './App.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Main from './components/Main';
+import NavBar from './components/NavBar';
 
 
 function App() {
   return (
   <BrowserRouter>
+      <NavBar></NavBar>
       <Routes>
         <Route
           path="/"
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { ImCart } from "react-icons/im";
+import { Link } from "react-router-dom";
+
+
+function NavBar()  {
+    //console.log({cartNum});
+    return (
+        
+        <div className="navBar">
+            <Link to="/" className="navBarItem"><span> &nbsp;&nbsp;<img className='logo' src="https://i.pinimg.com/564x/8d/9a/00/8d9a009dba6c959f892d4a834d450f98.jpg"></img>&nbsp;&nbsp; </span> </Link>
+            <Link to="/proizvodi" className="navBarItem">Our products</Link>
+            <Link to="/kontakt" className="navBarItem">Contact</Link>
+            <ImCart  style={{ marginLeft: 10 }} />
+            <Link to="/upiti" className="cart-items">
+                
+                <div className='cart-num'></div>
+            </Link>
+        </div>
+
+    )
+}
+
+export default NavBar
