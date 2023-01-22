b5404118964553b2fbe85961aa6e319f9413093c
jelena <repac01jelena@gmail.com>
2023-01-22 21:54:51 +0100

    Added main component and css for main

---
+++ src/components/Main.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+
+function Main() {
+
+    return (
+        <>
+        
+        <img className='logoImg' src="https://i.pinimg.com/564x/8d/9a/00/8d9a009dba6c959f892d4a834d450f98.jpg"></img>
+        
+        <div className='gridDiv'>
+            <div className='image'>
+            <img className='portfolioImg' src="https://i.pinimg.com/564x/b0/52/ba/b052ba4e577fb4430826345a745e4524.jpg"></img>
+            <img className='portfolioImg' src="https://i.pinimg.com/564x/0c/7d/3e/0c7d3ec8cbb21725c5fcf382d6537678.jpg"></img>
+            
+           
+            <p>They are exclusive 925 Sterling Silver pieces, plated in 22 karat gold. All the pieces are plated with 2 microns of thickness, and the rings with 3 microns. 
+                This guarantees the maximum purity and a long-lasting, with a flawless finish.</p>
+                    
+            </div>
+            <div className='desc'>
+            <p><b>A R Q A</b> All gold plated jewelry is based on our own designs. We wanted to maintain the essence and style of Arqa, 
+            so that you continue to enjoy and wear our jewels. In the collection you will find gold jewelry handmade with the same love and care as always. 
+            You will shine with these jewelry!
+             And remember, if you need anything, we will be happy to advise and help you.</p>
+  <img className='portfolioImg' src='https://i.pinimg.com/564x/3f/09/3d/3f093d76ca4e597086425093776254e2.jpg'></img>
+  <img className='portfolioImg' src='https://i.pinimg.com/564x/53/e5/c8/53e5c87f692d2bb86efc1e51e99e1bb0.jpg'></img>
+  
+          
+            </div>
+
+            
+        </div>
+        </>
+    )
+}
+
+export default Main
